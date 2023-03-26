71f78f82087a7122957eaaf6dfd37b10336def0f
Andrei Kalabin <a.kalabin@inbox.ru>
2023-03-26 21:04:12 +0500

    changed permissions

--- src/test/java/ru/avkalabin/AttachmentsTest.java
+++ src/test/java/ru/avkalabin/AttachmentsTest.java
@@ -1,12 +1,10 @@
 package ru.avkalabin;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.byLinkText;
-import static com.codeborne.selenide.Selectors.withText;
+
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.attachment;
 import static io.qameta.allure.Allure.step;
